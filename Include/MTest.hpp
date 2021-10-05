a8a95cd5899b09a8893def0624b5bbf785278f57
MatiRg <matehes@gmail.com>
2021-10-05 07:11:47 +0200

    Added Color Support

--- Include/MTest.hpp
+++ Include/MTest.hpp
@@ -30,6 +30,29 @@ For more information, please refer to <https://unlicense.org>
 #include <functional>
 #include <vector>
 #include <algorithm>
+#include <ostream>
+
+// _WIN64   - 64 bit
+// _WIN32   - 32 i 64 bit
+// _M_X64   - x64 Visual Studio
+// __CYGWIN__   - Windows with Cygwin (POSIX)
+//#if defined(__WIN32__) || defined(WIN32) || defined(_WIN32) || defined(__CYGWIN__) || defined(__MINGW32__)
+// __linux__     - Linux kernel
+// __x86_64__    - x64 GNUC
+//#elif defined(__LINUX__) || defined(linux) || defined(__linux) || defined(__linux__)
+
+#if defined(_WIN64) || defined(_WIN32)
+    #define WINDOWS_PLATFORM 1
+#elif defined(__linux__) || defined(linux)
+    #define LINUX_PLATFORM 1
+#else
+    #error "Unsupported platform"
+#endif
+
+#ifdef WINDOWS_PLATFORM
+    #define WIN32_LEAN_AND_MEAN
+    #include <windows.h>
+#endif // WINDOWS_PLATFORM
 
 // Macros for output
 #ifndef LOG
@@ -40,7 +63,7 @@ For more information, please refer to <https://unlicense.org>
 #define NEW_LINE std::endl
 #endif // NEW_LINE
 
-#define INFO(Msg) LOG << "[Info]: " << Msg << NEW_LINE
+#define INFO(Msg) LOG << MTest::EConsoleColor::Default << "[Info]: " << Msg << NEW_LINE
 
 /// Stops Unit test Execution when fails. Must evaluate to true expression.
 #define ASSERT_TRUE(Cond) \
@@ -71,7 +94,7 @@ do \
     if( !(Cond) ) \
     { \
         TEST_REF_UT.OnCheckFail(); \
-        LOG << MTest::CCheck( #Cond , __FILE__ , __LINE__ ) << NEW_LINE; \
+        LOG << MTest::EConsoleColor::Yellow << MTest::CCheck( #Cond , __FILE__ , __LINE__ ) << MTest::EConsoleColor::Default << NEW_LINE; \
     } \
 } \
 while(false)
@@ -83,7 +106,7 @@ do \
     if( (Cond) ) \
     { \
         TEST_REF_UT.OnCheckFail(); \
-        LOG << MTest::CCheck( #Cond , __FILE__ , __LINE__ ) << NEW_LINE; \
+        LOG << MTest::EConsoleColor::Yellow << MTest::CCheck( #Cond , __FILE__ , __LINE__ )<< MTest::EConsoleColor::Default << NEW_LINE; \
     } \
 } \
 while(false)
@@ -106,7 +129,7 @@ void Name##Sec##Test(MTest::CTestManager::CUnitTest& TEST_REF_UT)
 
 /// Use to Implement main()
 #define MTEST_MAIN \
-int main(int argc, char* argv[]) \
+int main(int, char*[]) \
 { \
     MTest::CTestManager::Instance().Run(); \
 	return 0; \
@@ -114,20 +137,75 @@ int main(int argc, char* argv[]) \
 
 namespace MTest
 {
+    enum class EConsoleColor
+    {
+        Default,
+        Red,
+        Yellow,
+        Green,
+        Blue
+    };
+
     namespace Detail
     {
         inline std::string GetFileName(const std::string& Path)
         {
             const auto Pos = Path.find_last_of( "\\/" );
-            if( Pos == std::string::npos ) 
+            if( Pos == std::string::npos )
             {
                 return Path;
             }
-            else 
+            else
             {
                 return Path.substr( Pos+1 );
             }
         }
+
+        int ToWin32Color(const EConsoleColor x)
+        {
+            switch(x)
+            {
+            case EConsoleColor::Red:
+                return 12;
+            case EConsoleColor::Yellow:
+                return 14;
+            case EConsoleColor::Green:
+                return 10;
+            case EConsoleColor::Blue:
+                return 9;
+            case EConsoleColor::Default:
+            default:
+                return 15;
+            }
+        }
+
+        std::string ToAnsiColor(const EConsoleColor x)
+        {
+            switch(x)
+            {
+            case EConsoleColor::Red:
+                return "\033[31m";
+            case EConsoleColor::Yellow:
+                return "\033[33m";
+            case EConsoleColor::Green:
+                return "\033[32m";
+            case EConsoleColor::Blue:
+                return "\033[34m";
+            case EConsoleColor::Default:
+            default:
+                return "\033[0m";
+            }
+        }
+    }
+
+    std::ostream& operator<<(std::ostream& Stream, const EConsoleColor x)
+    {
+        #ifdef WINDOWS_PLATFORM
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), Detail::ToWin32Color(x));
+        #else
+        Stream << Detail::ToAnsiColor(x);
+        #endif // WINDOWS_PLATFORM
+        return Stream;
     }
 
     class ICheck
@@ -183,7 +261,7 @@ namespace MTest
         }
     };
 
-    class CTestManager
+    class CTestManager final
     {
     public:
         class CUnitTest;
@@ -208,8 +286,8 @@ namespace MTest
             void Run()
             {
                 LOG << NEW_LINE;
-                LOG << "[Test]: " << Name << NEW_LINE;
-                LOG << "[Test]: File: " << Detail::GetFileName(File) << " Line: " << Line << NEW_LINE;
+                LOG << EConsoleColor::Blue << "[Test]: " << Name << EConsoleColor::Default << NEW_LINE;
+                LOG << EConsoleColor::Blue << "[Test]: File: " << Detail::GetFileName(File) << " Line: " << Line << EConsoleColor::Default << NEW_LINE;
                 try
                 {
                     Func(*this);
@@ -217,31 +295,31 @@ namespace MTest
                 catch(const CAssert& As)
                 {
                     Fail();
-                    LOG << As << NEW_LINE;
+                    LOG << EConsoleColor::Red << As << EConsoleColor::Default << NEW_LINE;
                 }
                 catch(const std::exception& Exception)
                 {
                     Fail();
-                    LOG << "[Exception]: " << Exception.what() << NEW_LINE;
+                    LOG << EConsoleColor::Red << "[Exception]: " << Exception.what() << EConsoleColor::Default << NEW_LINE;
                 }
                 catch(...)
                 {
                     Fail();
-                    LOG << "[Exception]: " << "Unknown" << NEW_LINE;
+                    LOG << EConsoleColor::Red << "[Exception]: Unknown" << EConsoleColor::Default << NEW_LINE;
                 }
-                if( !IsFailed() ) 
+                if( !IsFailed() )
                 {
-                    LOG << "[Test]: " << Name << " - Success" << NEW_LINE;
+                    LOG << EConsoleColor::Green << "[Test]: " << Name << " - Success" << EConsoleColor::Default << NEW_LINE;
                 }
                 else
                 {
-                    if( !ChecksFailed ) 
+                    if( !ChecksFailed )
                     {
-                        LOG << "[Test]: " << Name << " - Fail" << NEW_LINE;
+                        LOG << EConsoleColor::Red << "[Test]: " << Name << " - Fail" << EConsoleColor::Default << NEW_LINE;
                     }
-                    else 
+                    else
                     {
-                        LOG << "[Test]: " << Name << " - Fail - Failed Checks - " << ChecksFailed << NEW_LINE;
+                        LOG << EConsoleColor::Red << "[Test]: " << Name << " - Fail - Failed Checks - " << ChecksFailed << EConsoleColor::Default << NEW_LINE;
                     }
                 }
             }
@@ -263,7 +341,7 @@ namespace MTest
 
             void OnCheckFail()
             {
-                if( !Failed ) 
+                if( !Failed )
                 {
                     Fail();
                 }
@@ -296,10 +374,10 @@ namespace MTest
 
         void Add(const std::string& Name, const std::string& File, const int Line, const UnitTestCallback& Func)
         {
-            auto It = std::find_if(UnitTests.begin(), UnitTests.end(), [&](const CUnitTest& Other) { return Other.GetName() == Name; }); 
+            auto It = std::find_if(UnitTests.begin(), UnitTests.end(), [&](const CUnitTest& Other) { return Other.GetName() == Name; });
             if( It != UnitTests.end() )
             {
-                LOG << "[Error]: Test: " << Name << " Already Exists" << NEW_LINE;
+                LOG << EConsoleColor::Red << "[Error]: Test: " << Name << " Already Exists" << EConsoleColor::Default << NEW_LINE;
                 return;
             }
             UnitTests.emplace_back( Name, File, Line, Func );
@@ -307,11 +385,11 @@ namespace MTest
 
         void Run()
         {
-            LOG << "[Tests]: Count: " << UnitTests.size() << NEW_LINE;
+            LOG << EConsoleColor::Blue << "[Tests]: Count: " << UnitTests.size() << EConsoleColor::Default << NEW_LINE;
             for(auto& i: UnitTests)
             {
                 i.Run();
-                if( i.IsFailed() ) 
+                if( i.IsFailed() )
                 {
                     FailedUnitTest.push_back( i.GetName()+"; File: "+Detail::GetFileName(i.GetFile())+" Line: "+std::to_string(i.GetLine()) );
                 }
@@ -319,10 +397,10 @@ namespace MTest
             if( FailedUnitTest.size() )
             {
                 LOG << NEW_LINE;
-                LOG << "[Tests]: Failed Unit Tests: " << FailedUnitTest.size() << NEW_LINE;
+                LOG << EConsoleColor::Red << "[Tests]: Failed Unit Tests: " << FailedUnitTest.size() << EConsoleColor::Default << NEW_LINE;
                 for(const auto& i: FailedUnitTest)
                 {
-                    LOG << "[Test]: " << i << NEW_LINE;
+                    LOG << EConsoleColor::Red << "[Test]: " << i << EConsoleColor::Default << NEW_LINE;
                 }
             }
         }
@@ -332,10 +410,25 @@ namespace MTest
         CTestManager& operator=(const CTestManager&) = delete;
         CTestManager& operator=(CTestManager&&) = delete;
     private:
-        CTestManager() = default;
+        CTestManager()
+        {
+            #ifdef WINDOWS_PLATFORM
+            GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+            #endif // WINDOWS_PLATFORM
+        }
+
+        ~CTestManager()
+        {
+            #ifdef WINDOWS_PLATFORM
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), csbi.wAttributes);
+            #endif // WINDOWS_PLATFORM
+        }
     private:
         std::vector<CUnitTest> UnitTests;
         std::vector<std::string> FailedUnitTest;
+        #ifdef WINDOWS_PLATFORM
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+        #endif // WINDOWS_PLATFORM
     };
 
     struct STestProxy
